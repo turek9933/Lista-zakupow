6c6206d4eb0f3b548a9776503439b75eaaa1ada3
turek9933 <turek9933@gmail.com>
2025-05-22 21:28:33 +0200

    Aktualizacja Firebase config oraz gitignore

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { firebaseConfig } from "@/src/constants/firebase.env";
+import { firebaseConfig } from "@/src/constants/firebase";
 
 // https://console.firebase.google.com/
 // fireConfig
